777f755ee2308504024afb368f5625a50e91f593
emmanuelJd <elisa.emmanuel.jd@gmail.com>
2016-12-12 22:45:42 +0100

    start to initialize test env.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rockfallTools
+
+R&D about rockfall project, implement some keys functions like detect horizontal, vertical and diagonal win.
---
+++ tests/horizontalAlignTest.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+var plate = require('./_initialData');
+
+console.log(plate);
+
+describe('check horizontal align', function() {
+  it('for 4 pieces to win, it should return true in win object', function() {
+    //to define a win align 4 pieces
+  });
+});
+
+describe('check horizontal align', function() {
+  it('for 4 pieces to win, it should return false in win object', function() {
+    //to define a win align 4 pieces
+  });
+});
