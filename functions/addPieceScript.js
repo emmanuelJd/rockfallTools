ffb4c1dcdaa368d2a8779c7a20111c359f783cec
ELISA <emmanuel.elisa.external@sigfox.com>
2017-01-06 18:53:22 +0100

    still working on it

--- functions/addPieceScript.js
+++ functions/addPieceScript.js
@@ -1,16 +1,16 @@
 /**
 * function to add a piece to the boardGame
 */
-
-function addPieceScript(boardGame, indexPiece, piece) {
+function addPieceScript(boardGame, indexPiece, piece, users) {
 
   if(!piece.hasOwnProperty("x") && !piece.hasOwnProperty("users")) throw new Error("not validate piece!");
   if (!boardGame.length) throw new Error("undefined boardGame!");
   if (!boardGame[0].length) throw new Error("undefined boardGame!");
-
-  if(boardGame[0][piece.y] == 0){
+// if you want to add a piece, you need to have an action available and it's your turn to play 
+  if(boardGame[0][piece.y] == 0 && users[piece.users].addAction && users[piece.users].turnToPlay){
     piece.y = 0;
     boardGame[0][piece.y] = piece;
     indexPiece.add(piece);
+    users[piece.users].addAction -= 1;
   }
 }
